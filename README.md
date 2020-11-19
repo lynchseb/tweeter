cac05c75b2544c0ffb42bd68050ec283d28c68b0
Sebastian <lynchseb@gmail.com>
2020-11-19 22:12:57 +0000

    implement responsive design and readme.md

--- README.md
+++ README.md
@@ -4,14 +4,25 @@ Tweeter is a simple, single-page Twitter clone.
 
 This repository is the starter code for the project: Students will fork and clone this repository, then build upon it to practice their HTML, CSS, JS, jQuery and AJAX front-end skills, and their Node, Express and MongoDB back-end skills.
 
+## Final Product
+
+!["Error Message"](https://github.com/lynchseb/tweeter/blob/master/docs/ErrorMessage.png?raw=true)
+!["Main Page"](https://github.com/lynchseb/tweeter/blob/master/docs/mainPage.png?raw=true)
+!["Responsive Counter"](https://github.com/lynchseb/tweeter/blob/master/docs/responsiveCounterShot.png?raw=true)
+!["Responsive"](https://github.com/lynchseb/tweeter/blob/master/docs/responsiveShot.png?raw=true)
+
 ## Getting Started
 
 1. Fork this repository, then clone your fork of this repository.
 2. Install dependencies using the `npm install` command.
 3. Start the web server using the `npm run local` command. The app will be served at <http://localhost:8080/>.
 4. Go to <http://localhost:8080/> in your browser.
+5. Please note the fancy fadeIn/fadeOut stretch button. 
 
 ## Dependencies
 
 - Express
+- Body-parser
+- Chance
 - Node 5.10.x or above
+
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,11 +2,116 @@
  * Client-side JS logic goes here
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+ */  
 
+//Helper Functions
+const dayConverter = function(milliseconds) {
+  let difference = (new Date() - milliseconds);
+  let minutes = Math.floor(difference / 60000);
+  let hours = Math.round(minutes / 60);
+  let days = Math.round(hours / 24);
+  const date = {days, hours, minutes};
+  return date;
+};
+console.log(dayConverter(1461116232227))
+
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+const renderTweets = function(data){
+  $(".tweets-container").empty();
+  data.forEach((tweets) => {
+    const tweet = createTweetElement(tweets)
+    $(".tweets-container").prepend(tweet);
+  })
+}
+
+const createTweetElement = function(tweet) {
+  let conversion = dayConverter(tweet.created_at)
+  let { days } = conversion;
+  return tweet = $(`
+  <article class="tweet">
+  <header class="articleHead">
+    <div class="articleHead">
+      <img class="marginRight" src=${tweet.user.avatars} alt="abc">
+      <p>${tweet.user.name}</p>
+    </div>
+    <div class="hiddenName">${tweet.user.handle}</p>
+    </div>
+  </header>
+  <p class="tweetContent">
+    ${escape(tweet.content.text)}
+  </p>
+  <hr />
+  <footer>
+    <div class="footContent">
+      <a>${days} days ago</a>
+      <a>
+      <i class="fas fa-flag"></i>
+      <i class="fas fa-retweet"></i>
+      <i class="fas fa-heart"></i>
+      <a/>
+    </div>
+  </footer>
+</article>`); 
+}
+
+const loadTweets = function() {
+  $.ajax({
+    url: '/tweets',
+    data: 'json'
+  })
+  .then(data => {
+    renderTweets(data)
+  })
+}
+
+const isTweetTooLong = function(message) {
+return message.length > 140;
+}
+
+const isTweetNotValid = function(message) {
+ return message === "";
+}
+
+const scrollTop = () => {
+  $(window).scroll(function() {
+   if ($(this).scrollTop() > 300) {
+     $(".topBtn").fadeIn();
+   } setTimeout(function() {
+    if ($(this).scrollTop() < 300){
+      $(".topBtn").fadeOut();
+    }
+   }, 1000)
+   
+ });
+ $(".topBtn").click(function() {
+   $('html').animate({scrollTop: 0}, 1000);
+   if($(".new-tweet").is(":visible")) {
+    $(".new-tweet textarea").focus();
+   } else {
+     setTimeout(function(){
+       $(".new-tweet").toggle("slow");
+       $(".new-tweet textarea").focus();
+     }, 1000)
+   }
+ });
+}
+
+const navBarToggle = () => {
+  $(".rightMenuArrow").click(function() {
+    $(".new-tweet").toggle("slow");
+    $("textarea").focus();
+  });
+}
+//Main Document
 $(document).ready(function() {
  console.log("ready!");
 
+//Tweet Counter
   $("#tweet-form textarea").on('input', function() {
     const tweetLength = this.value.length;
     $(this).parent().find(".counter").text(140 - tweetLength);
@@ -18,7 +123,41 @@ $(document).ready(function() {
     }
   });
 
-  // loadTweets();
+//Tweet submission form
+  $('#tweet-form').submit(function(event) {
+    event.preventDefault();
+    const message = $('#tweet-form textarea').val()
+    if (isTweetTooLong(message)){
+      $(".alert h3").text(`Too long: please remove ${message.length - 140} characters`);
+      $(".alert").slideDown('slow');
+      setTimeout(function(){
+         $(".alert").slideUp('slow');
+       }, 3000)
+    } else if (isTweetNotValid(message)){
+      $(".alert h3").text("Not a valid Tweet");
+      $(".alert").slideDown('slow');
+      setTimeout(function(){
+        $(".alert").slideUp('slow');
+      }, 3000)
+    } else {
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      })
+      .then(() => { 
+       $(".counter").text(140);
+       $("#tweet-form").trigger('reset');
+       $(".new-tweet").toggle("slow");
+       loadTweets();
+       $(".tweet").focus();
+      })
+    }
+  })
+// Call functions on Document.ready
+loadTweets();
+scrollTop();
+navBarToggle();
 });
  
 
